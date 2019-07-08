4140eeffa452569cb992dfc1cf44478214a1db3a
Marko-Misko <miskovic.m@yahoo.com>
2019-07-08 09:12:55 +0200

    Gotovo

--- TVShows/TVShows/Login/LoginViewController.swift
+++ TVShows/TVShows/Login/LoginViewController.swift
@@ -11,4 +11,46 @@ import UIKit
 class LoginViewController : UIViewController
 {
     
+    var tapCounter = 0
+    
+    @IBOutlet weak var myActivityIndicator: UIActivityIndicatorView! {
+        didSet {
+            myActivityIndicator.startAnimating()
+            DispatchQueue.main.asyncAfter(deadline: .now() + 3) {
+               self.myActivityIndicator.stopAnimating()
+            }
+        }
+    }
+    
+    @IBOutlet weak var myButton: UIButton! {
+        didSet {
+            myButton.backgroundColor = .green
+            myButton.layer.cornerRadius = 20
+            myButton.frame.size = CGSize(width: 120.0, height: 60.0)
+            
+            let icon = UIImage(named: "Plus") as UIImage?
+            myButton.setImage(icon, for: .normal)
+            myButton.setTitle("Increment!", for: .normal)
+            myButton.sizeToFit()
+        }
+    }
+    
+    @IBOutlet weak var myLabel: UILabel!  {
+        didSet {
+            myLabel.text = "\(tapCounter)"
+            myLabel.font = UIFont(name: "Verdana-BoldItalic", size: 45.0)
+        }
+    }
+    
+    @IBAction func touchUpInside(_ sender: Any) {
+        print("It works!")
+        tapCounter += 1
+        myLabel.text = "\(tapCounter)"
+        myActivityIndicator.isAnimating ? myActivityIndicator.stopAnimating() : myActivityIndicator.startAnimating()
+    }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        self.view.backgroundColor = .cyan
+    }
 }

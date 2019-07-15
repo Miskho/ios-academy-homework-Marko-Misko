34b258dc2b5e4b72a6a9ca027b64ee9c7cfb29e2
Marko-Misko <miskovic.m@yahoo.com>
2019-07-15 13:39:48 +0200

    Laid out interface - trailing edge not yet fully designed

--- TVShows/TVShows/Modules/Login/LoginViewController.swift
+++ TVShows/TVShows/Modules/Login/LoginViewController.swift
@@ -10,44 +10,4 @@ import UIKit
 
 class LoginViewController : UIViewController {
     
-    private var numberOfTaps = 0
-    
-    @IBOutlet weak var myActivityIndicator: UIActivityIndicatorView! {
-        didSet {
-            myActivityIndicator.startAnimating()
-            DispatchQueue.main.asyncAfter(deadline: .now() + 3) {
-               self.myActivityIndicator.stopAnimating()
-            }
-        }
-    }
-    
-    @IBOutlet weak var myButton: UIButton! {
-        didSet {
-            myButton.layer.cornerRadius = 20
-            myButton.frame.size = CGSize(width: 120.0, height: 60.0)
-            
-            myButton.setImage(UIImage(named: "Plus"), for: .normal)
-            myButton.setTitle("Increment!", for: .normal)
-            myButton.sizeToFit()
-        }
-    }
-    
-    @IBOutlet weak var myLabel: UILabel!  {
-        didSet {
-            myLabel.text = "\(numberOfTaps)"
-            myLabel.font = UIFont(name: "Verdana-BoldItalic", size: 45.0)
-        }
-    }
-    
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        view.backgroundColor = .cyan
-    }
-    
-    @IBAction func buttonPressed() {
-        print("It works!")
-        numberOfTaps += 1
-        myLabel.text = "\(numberOfTaps)"
-        myActivityIndicator.isAnimating ? myActivityIndicator.stopAnimating() : myActivityIndicator.startAnimating()
-    }
 }
